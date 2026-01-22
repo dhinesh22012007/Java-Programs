dae205f4be1eff80facd47bb702879738d3b117c
dhinesh22012007 <dhinesh22012007@gmail.com>
2026-01-22 19:42:08 +0530

    Add files via upload

---
+++ IncrementDecrement.java
@@ -0,0 +1,13 @@
+public class IncrementDecrement {
+    public static void main(String args[])
+    {
+        byte m=2;
+        System.out.println(m++);
+        System.out.println(m++);
+        System.out.println(m);
+        System.out.println(--m);
+        System.out.println(m--);
+        System.out.println(m);
+    }
+    
+}
