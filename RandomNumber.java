fd2fd6fe2f8a1186192c204d2a889eee9d40b216
Dhinesh S <dhinesh22012007@gmail.com>
2026-03-08 21:26:19 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,7 @@
+// generate the random Number from 0-99;
+public class RandomNumber {
+    public static void main(String[] args) {
+        int x=(int)(Math.random()*100);
+        System.out.println(x);
+    }
+}
