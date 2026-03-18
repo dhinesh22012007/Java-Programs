542fabd7870692599b6c1012308b34c9c2455c06
Dhinesh S <dhinesh22012007@gmail.com>
2026-03-18 22:37:16 +0530

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,15 @@
+//Print 2D Array 
+
+public class Array2D {
+    public static void main(String[] args) {
+        int arr[][]={ {10,20,30}, {40,50,60}, {70,80,90}};
+
+        for(int i=0;i<arr.length;i++){
+            
+            for(int j=0;j<arr.length;j++){
+                System.out.print(" "+arr[i][j]);
+            }
+            System.out.println(" ");
+        }
+    }
+}
