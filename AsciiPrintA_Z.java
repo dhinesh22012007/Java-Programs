21c3c2ee89f0b29c743a6ee667c9c2dcbe70e71b
Dhinesh S <dhinesh22012007@gmail.com>
2026-03-25 19:53:38 +0530

    Add files via upload

---
+++ AsciiPrintA_Z.java
@@ -0,0 +1,10 @@
+//print the letter A - Z using the Ascii value.
+public class AsciiPrintA_Z {
+    public static void main(String[] args) {
+        
+        for(int i=65;i<=90;i++)
+        {
+            System.out.print(i+"-"+(char)i+"  ");
+        }
+    }
+}
---
+++ Ascii_a_z.java
@@ -0,0 +1,10 @@
+//print the letter a - z using the Ascii value.
+public class Ascii_a_z{
+    public static void main(String[] args) {
+        
+        for(int i=97;i<=122;i++)
+        {
+            System.out.print(i+"-"+(char)i+"  ");
+        }
+    }
+}
